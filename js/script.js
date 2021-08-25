6617b4719b90084c427f5e0b8622a0f018529099
joatansampaio <76176945+joatansampaio@users.noreply.github.com>
2021-08-25 16:05:39 -0500

    update

--- js/script.js
+++ js/script.js
@@ -68,10 +68,11 @@ startButton.addEventListener("click", startGame);
 
 function smash(e) {
    gameScore++;
-   this.style.backgroundImage = "url('images/smashed.png')";
+   this.style.backgroundImage =
+      "url('https://raw.githubusercontent.com/joatansampaio/SmackGame/main/images/smashed.png')";
    this.style.pointerEvents = "none";
    setTimeout(() => {
-      this.style.backgroundImage = "url('images/character.png')";
+      this.style.backgroundImage = "url('/images/character.png')";
       this.style.pointerEvents = "all";
    }, 800);
    scoreText.textContent = gameScore;
