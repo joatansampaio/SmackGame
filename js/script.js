53d971caf52ea51b1a41848e0f74d821035d8671
joatansampaio <76176945+joatansampaio@users.noreply.github.com>
2021-08-25 15:47:48 -0500

    background bugfix

--- js/script.js
+++ js/script.js
@@ -68,10 +68,10 @@ startButton.addEventListener("click", startGame);
 
 function smash(e) {
    gameScore++;
-   this.style.backgroundImage = "url(./images/smashed.png)";
+   this.style.backgroundImage = "url('../images/smashed.png')";
    this.style.pointerEvents = "none";
    setTimeout(() => {
-      this.style.backgroundImage = "url(./images/character.png)";
+      this.style.backgroundImage = "url('../images/character.png')";
       this.style.pointerEvents = "all";
    }, 800);
    scoreText.textContent = gameScore;
